b7794adb8ff42051866fa45a0dd82faabf9d83bb
Esen <esenbekm03@gmail.com>
2022-01-15 13:52:00 +0600

    FP

---
+++ calc.py
@@ -0,0 +1,24 @@
+
+while True:
+    a = float(input("Введите первое число: "))
+    b = (input("Введите операцию(+, -, *, /): "))
+    c = float(input("Введите второе число: "))
+
+    if b=="+":
+        answer = a+c
+    elif b=="-":
+        answer = a-c 
+    elif b=="*":
+        answer = a*c
+    elif b=="pow":
+        answer = a**c
+
+    elif b=="/" and c==0:
+        print("Нельза делить на 0!")
+    elif b=="/":
+        answer = a/c
+
+    else:
+        print("Не опознанная операция")
+
+    print(f"Вывод: {answer}")
---
+++ guess_num.py
@@ -0,0 +1,25 @@
+import random
+
+number = random.randint(1, 100)
+count = int(input("Сколько попыток вам нужно? "))
+
+print(number)
+
+while count != 0:
+    count -= 1
+    try:
+        guess = int(input('Отгадайте число: '))
+    except ValueError:
+        print('Вводите целые числа!')
+        continue    
+    if guess == number:
+        print(f'Поздровляю, вы угадали!')
+        break
+    elif guess < number:
+        print('Больше')
+        print(f'У вас осталось {count} попыток!')
+    elif guess > number:
+        print('Меньше')
+        print(f'У вас осталось {count} попыток!')
+
+print('Игра окончена!')
